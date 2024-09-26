a2b62c62f110c92ebeec022d271e7bbd3b4ed8ba
MagicIrfan <bouhenaf.irfan@gmail.com>
2024-09-26 16:27:23 +0200

    adding main menu

--- BattleShip.Models/Position.cs
+++ BattleShip.Models/Position.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace BattleShip.Models;
 
-namespace BattleShip.Models
+public enum Position
 {
-    internal class Position
-    {
-    }
+    HORIZONTAL,
+    VERTICAL
 }
