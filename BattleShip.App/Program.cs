35ec34748c087dc36d9eccbd3702554663f41544
MagicIrfan <bouhenaf.irfan@gmail.com>
2024-09-26 14:50:04 +0200

    first commit

--- BattleShip.App/Program.cs
+++ BattleShip.App/Program.cs
@@ -1,6 +1,6 @@
-using BattleShip;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BattleShip.App;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
