c7830e3936ba90892173752253546f99ff807c1e
MagicIrfan <bouhenaf.irfan@gmail.com>
2024-09-27 10:32:13 +0200

    adding start game

--- BattleShip.Models/Position.cs
+++ BattleShip.Models/Position.cs
@@ -1,15 +1,8 @@
 ﻿namespace BattleShip.Models;
 
-<<<<<<< HEAD
 public class Position(int x, int y)
 {
     public int X { get; set; } = x;
     public int Y { get; set; } = y;
     public bool IsHit { get; set; } = false;
-=======
-public enum Position
-{
-    HORIZONTAL,
-    VERTICAL
->>>>>>> d899fce32d218d0f991ee2b2fa5d4815a9496c48
 }
