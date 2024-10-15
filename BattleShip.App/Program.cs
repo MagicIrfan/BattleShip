f70c4b7882bbae0d39e1cb664eefed4df24e8d3f
MagicIrfan <bouhenaf.irfan@gmail.com>
2024-10-15 08:38:25 +0200

    a

--- BattleShip.App/Program.cs
+++ BattleShip.App/Program.cs
@@ -5,6 +5,7 @@ using Grpc.Net.Client.Web;
 using Grpc.Net.Client;
 using BattleShip;
 using BattleShip.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,8 +13,16 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazoredModal();
 
+/*builder.Services.AddMsalAuthentication(options => {
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("profile");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("email");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://YOUR_AUTH0_DOMAIN/userinfo");
+});*/
+
 builder.Services.AddScoped<IGameModalService, GameModalService>();
-builder.Services.AddScoped<IGameService, BattleShip.Services.GameService>();
+builder.Services.AddScoped<IGameService, GameService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp =>
